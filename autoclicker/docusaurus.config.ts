6acd9f1281befa9c812fa151317c4287c4a8273a
kim <kimliss117@gmail.com>
2024-09-03 18:41:47 +0800

    chore: faq

--- autoclicker/docusaurus.config.ts
+++ autoclicker/docusaurus.config.ts
@@ -1,56 +1,55 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+import type { Options as DocsOptions } from "@docusaurus/plugin-content-docs";
 
 const config: Config = {
-  title: 'GA Auto Clicker',
-  tagline: 'Free Your Hands',
-  favicon: 'img/favicon.ico',
+  title: "GA Auto Clicker",
+  tagline: "Free Your Hands",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://autoclicker.godlikeassist.com',
+  url: "https://autoclicker.godlikeassist.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   // organizationName: 'facebook', // Usually your GitHub org/user name.
   // projectName: 'docusaurus', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en','zh-Hans', 'zh-Hant'],
+    defaultLocale: "en",
+    locales: ["en", "zh-Hans", "zh-Hant"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
-        docs: {
-          sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-        },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
+        // docs: {
+        //   sidebarPath: "./sidebars.ts",
+        // },
+        // blog: {
+        //   showReadingTime: true,
+        //   feedOptions: {
+        //     type: ["rss", "atom"],
+        //     xslt: true,
+        //   },
+        //   // Useful options to enforce blogging best practices
+        //   onInlineTags: "warn",
+        //   onInlineAuthors: "warn",
+        //   onUntruncatedBlogPosts: "warn",
+        // },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -58,72 +57,35 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    // image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: 'GA Auto Clicker',
+      title: "GA Auto Clicker",
       logo: {
-        alt: 'GA Auto Clicker',
-        src: 'img/logo.png',
+        alt: "GA Auto Clicker",
+        src: "img/logo.png",
       },
       items: [
+        // {
+        //   type: "docSidebar",
+        //   sidebarId: "docsSidebar",
+        //   position: "left",
+        //   label: "Docs",
+        // },
+        { to: "/faq", label: "FAQ", position: "left"},
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
+          href: "https://play.google.com/store/apps/details?id=com.ga.speed.automatictap.autoclicker.clicker",
+          label: "Google Play",
+          position: "right",
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://play.google.com/store/apps/details?id=com.ga.speed.automatictap.autoclicker.clicker',
-          label: 'Google Play',
+          type: 'localeDropdown',
           position: 'right',
         },
       ],
     },
     footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      style: "dark",
+      copyright: `Copyright © ${new Date().getFullYear()} GodLikeAssist, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
